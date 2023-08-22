0f5fc8852a8f3fbedec4aee5a39af59c33df8bad
Diego-si <diventura1@gmail.com>
2023-08-22 19:49:22 -0300

    tipos primitivos pt 05

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+        String nome = "abelha";
+        String endereco = "av";
+        double salario = 1.0;
+        String dataSalario = "22/08/2023";
+        String relatorio = "Eu " + nome + " morando no endereço " + endereco +
+                " confirmo que recebi " + salario + ", na data " + dataSalario;
+        System.out.println(relatorio);
+    }
+
+}
