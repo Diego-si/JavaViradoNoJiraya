c4d8fdfd7bededcb18eeac73a6006ceff8cf7693
Diego-si <diventura1@gmail.com>
2023-08-22 19:30:41 -0300

    commit inicial com algumas alterações

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = (int) 10000000000L;
+        long numeroGrande = (long) 155.23;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041'; //unicode
+
+        System.out.println("A idade é " + idade + " anos.");
+        System.out.println("O caractere é " + caractere);
+    }
+}
