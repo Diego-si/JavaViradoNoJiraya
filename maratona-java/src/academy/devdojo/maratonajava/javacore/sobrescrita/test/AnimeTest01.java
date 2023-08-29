121ea52cbc8c6b80dbe4403f6ae9d79600b813bd
Diego-si <diventura1@gmail.com>
2023-08-29 10:21:58 -0300

    Orientação Objetos - Sobrescrita do método toString

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.sobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.sobrescrita.domino.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Anime");
+        System.out.println(anime);
+    }
+}
