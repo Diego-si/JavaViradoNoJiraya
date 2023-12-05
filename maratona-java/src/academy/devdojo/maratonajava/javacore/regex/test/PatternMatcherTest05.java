f49eedee68f72b73fe9d410e9d4d8d458978d430
Diego-si <diventura1@gmail.com>
2023-12-05 07:39:32 -0300

    Classes Utilitárias - Regex pt 05 - Pattern e Matcher - Quantificadores pt 02

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/regex/test/PatternMatcherTest05.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.regex.test;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest05 {
+    public static void main(String[] args) {
+        // \d = Todos os digitos
+        // \D = Tudo o que nao for dígito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracters excluindo os brancos
+        // \w = a-ZA-Z, dígitos, _
+        // \W = Tudo o que nao for incluso no \w
+        // []
+        // ? Zero ou uma
+        // * Zero ou mais
+        // + uma ou mais
+        // {n,m} de n até m
+        // ()
+        // |
+        // $
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)+";
+        String texto = "luffy@hotmail.com, 123jotaro@gmail.com, #@!zoro@mail.br, teste@gmail.com.br, sakura@gmail";
+        System.out.println("Email valido");
+        System.out.println("#@!zoro@mail.br".matches(regex));
+        System.out.println(texto.split(",")[1].trim());
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:  "+texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex: "+regex);
+        System.out.println("Posicoes encontradas");
+        while (matcher.find()) {
+            System.out.print(matcher.start()+" "+matcher.group()+"\n");
+        }
+    }
+}
