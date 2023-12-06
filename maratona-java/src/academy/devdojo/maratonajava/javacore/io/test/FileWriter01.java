24fd869f4723a105e93faf212560b1e201179c05
Diego-si <diventura1@gmail.com>
2023-12-06 06:50:20 -0300

    Classes Utilitárias - IO pt 02 - FileWriter

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/io/test/FileWriter01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.io.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        // append true para nao sobescrever
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write("alo galera de callgilr aloooo");
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
