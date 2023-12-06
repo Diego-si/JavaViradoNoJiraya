8108fb5b527698b61d7c604b3325f7131100dbe5
Diego-si <diventura1@gmail.com>
2023-12-06 07:21:05 -0300

    Classes Utilitárias - IO pt 05 - BufferedReader

--- maratona-java/src/academy/devdojo/maratonajava/javacore/io/test/BufferedWriterTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/io/test/BufferedWriterTest01.java
@@ -6,11 +6,11 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-            BufferedWriter br = new BufferedWriter(fw)) {
+            BufferedWriter bw = new BufferedWriter(fw)) {
             // BufferedWriter e mais rapido
-            br.write("alo galera de callgilr aloooo");
-            br.newLine(); // baseado no sistema operacional
-            br.flush(); // fazer limpeza
+            bw.write("alo galera de callgilr aloooo");
+            bw.newLine(); // baseado no sistema operacional
+            bw.flush(); // fazer limpeza
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
