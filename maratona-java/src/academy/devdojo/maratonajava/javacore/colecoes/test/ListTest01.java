bf9113f51cead1815acff3e845b10b7c336b942b
Diego-si <diventura1@gmail.com>
2023-12-11 07:29:30 -0300

    Coleções pt 07 - List pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/colecoes/test/ListTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/colecoes/test/ListTest01.java
@@ -7,16 +7,27 @@ public class ListTest01 {
     public static void main(String[] args) {
 //        List nomes = new ArrayList(); // 1.4
         List<String> nomes = new ArrayList<>(16); // 1.5
+        List<String> nomes2 = new ArrayList<>(16); // 1.5
         nomes.add("Digo");
         nomes.add("DevDojo Academy");
+        nomes2.add("aaaa");
+        nomes2.add("bbbb");
+//        System.out.println(nomes.remove("digo"));
+
+        nomes.addAll(nomes2);
 
         for (String nome: nomes) {
             System.out.println(nome);
         }
-        nomes.add("S");
+
         System.out.println("---");
-        for (int i = 0; i < nomes.size(); i++) {
+        int size = nomes.size();
+        for (int i = 0; i < size; i++) {
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        System.out.println(numeros);
     }
 }
