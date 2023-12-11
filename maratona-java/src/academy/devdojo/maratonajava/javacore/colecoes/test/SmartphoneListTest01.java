ba4e9ca7c3bcd6778b8831e6eb0b19313bb2aedd
Diego-si <diventura1@gmail.com>
2023-12-11 07:41:13 -0300

    Coleções pt 08 - List pt 03

--- maratona-java/src/academy/devdojo/maratonajava/javacore/colecoes/test/SmartphoneListTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/colecoes/test/SmartphoneListTest01.java
@@ -11,19 +11,21 @@ public class SmartphoneListTest01 {
         Smartphone s1 = new Smartphone("1ABC1", "iphone");
         Smartphone s2 = new Smartphone("22222", "Pixel");
         Smartphone s3 = new Smartphone("33333", "Samsung");
+        Smartphone s4 = new Smartphone("44444", "Xiaomi");
         List<Smartphone> smartphones = new ArrayList<>(6);
         smartphones.add(s1);
         smartphones.add(s2);
         smartphones.add(0, s3);
+        smartphones.add(s4);
 
 //        smartphones.clear(); // limpa tudo
         for (Smartphone smartphone : smartphones) {
             System.out.println(smartphone);
         }
-        Smartphone s4 = new Smartphone("22222", "Pixel");
+        Smartphone s5 = new Smartphone("22222", "Pixel");
 
-        System.out.println(smartphones.contains(s4));
-        int indexSmartphone4 = smartphones.indexOf(s4);
+        System.out.println(smartphones.contains(s5));
+        int indexSmartphone4 = smartphones.indexOf(s5);
         System.out.println(smartphones.get(indexSmartphone4));
     }
 }
