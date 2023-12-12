caa4e4ac4c692047200d98ba13508f5d45f0a12b
Diego-si <diventura1@gmail.com>
2023-12-12 06:34:43 -0300

    Coleções pt 11 - Sorting lists pt 03 - Comparator

--- maratona-java/src/academy/devdojo/maratonajava/javacore/colecoes/test/MangaSortTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/colecoes/test/MangaSortTest01.java
@@ -4,8 +4,14 @@ import academy.devdojo.maratonajava.javacore.colecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-
+class MangaByIdComparator implements Comparator<Manga> {
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
 public class MangaSortTest01 {
     public static void main(String[] args) {
         List<Manga> mangas = new ArrayList<>(6);
@@ -20,6 +26,14 @@ public class MangaSortTest01 {
         }
 
         Collections.sort(mangas);
+        System.out.println("----------");
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+//        Collections.sort(mangas, new MangaByIdComparator());
+        mangas.sort(new MangaByIdComparator());
         System.out.println("----------");
 
         for (Manga manga : mangas) {
