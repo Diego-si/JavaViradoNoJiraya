41e0ce9c86589fab22863958f30c10d582931d5d
Diego-si <diventura1@gmail.com>
2023-12-13 07:37:04 -0300

    Generics pt 03 - Wildcard pt 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/generics/test/WildcardTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/generics/test/WildcardTest02.java
@@ -1,17 +1,25 @@
 package academy.devdojo.maratonajava.javacore.generics.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatoes = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatoes);
+        List<Animal> animals = new ArrayList<>();
+        printCosultaAnimal(animals);
     }
     // Type erasure
-    private static void printConsulta(List<Animal> animals) {
+    private static void printConsulta(List<? extends Animal> animals) {
         for (Animal animal : animals) {
             animal.consulta();
         }
+    }
+    private static void printCosultaAnimal(List<? super Animal> animals) {
         animals.add(new Cachorro());
+        animals.add(new Gato());
     }
 }
