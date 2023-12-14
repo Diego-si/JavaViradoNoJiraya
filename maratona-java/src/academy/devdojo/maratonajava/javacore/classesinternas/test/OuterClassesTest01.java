b0ccbc6471cbde88a211251acc68b62b70c6c7aa
Diego-si <diventura1@gmail.com>
2023-12-14 06:39:16 -0300

    Classes Internas pt 02 - Classes Locais

--- maratona-java/src/academy/devdojo/maratonajava/javacore/classesinternas/test/OuterClassesTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/classesinternas/test/OuterClassesTest01.java
@@ -1,19 +1,19 @@
 package academy.devdojo.maratonajava.javacore.classesinternas.test;
 
-public class OutherClassesTest01 {
+public class OuterClassesTest01 {
     private String name = "Monkey D. Lufyy";
 
     class  Inner {
         public void printOutherClassAttribute() {
             System.out.println(name);
             System.out.println(this);
-            System.out.println(OutherClassesTest01.this);
+            System.out.println(OuterClassesTest01.this);
         }
     }
     public static void main(String[] args) {
-        OutherClassesTest01 outherClass = new OutherClassesTest01();
+        OuterClassesTest01 outherClass = new OuterClassesTest01();
         Inner inner = outherClass.new Inner();
-        Inner inner2 = new OutherClassesTest01().new Inner();
+        Inner inner2 = new OuterClassesTest01().new Inner();
         inner.printOutherClassAttribute();
         inner2.printOutherClassAttribute();
     }
