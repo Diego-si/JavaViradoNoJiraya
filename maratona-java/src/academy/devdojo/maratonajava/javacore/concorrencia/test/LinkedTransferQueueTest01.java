40f63377d7b3f0ec3a814ccb0e9d2321e011a1c1
Diego-si <diventura1@gmail.com>
2023-12-21 07:20:26 -0300

    Concorrência pt 07 - LinkedTransferQueue

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/concorrencia/test/LinkedTransferQueueTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.concorrencia.test;
+
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TransferQueue;
+
+public class LinkedTransferQueueTest01 {
+    public static void main(String[] args) throws InterruptedException {
+        TransferQueue<Object> tq = new LinkedTransferQueue<>();
+        System.out.println(tq.add("Aaaa"));
+        System.out.println(tq.offer("Bbbbb"));
+        System.out.println(tq.offer("Bbbbb", 10, TimeUnit.SECONDS));
+        tq.put("Dev");
+        if (tq.hasWaitingConsumer()) {
+            tq.transfer("Dev");
+        }
+        System.out.println(tq.tryTransfer("Academy"));
+        System.out.println(tq.tryTransfer("Academy", 5, TimeUnit.SECONDS));
+        System.out.println(tq.element());
+        System.out.println(tq.peek());
+        System.out.println(tq.poll());
+        System.out.println(tq.remove());
+        System.out.println(tq.take());
+        System.out.println(tq.remainingCapacity());
+    }
+}
